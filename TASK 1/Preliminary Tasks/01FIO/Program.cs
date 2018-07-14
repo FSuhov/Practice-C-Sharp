bc3b307f96b08be7f518fb93c6ff91b2ce40f7d6
Alex Brylov <aksu@ukr.net>
2018-07-14 14:08:39 +0300

    Task 1 completed

--- TASK 1/Preliminary Tasks/01FIO/Program.cs
+++ TASK 1/Preliminary Tasks/01FIO/Program.cs
@@ -8,8 +8,28 @@ namespace _01FIO
 {
     class Program
     {
+        const char stringStart = '+';
+        const char delimiter = '=';
+        const char separator = '|';
+        const string fio = "Сухов Федор Ильич";
         static void Main(string[] args)
         {
+            int length = fio.Length;
+            Console.Write(stringStart);
+            for(int i = 0; i < length; i++)
+            {
+                Console.Write(delimiter);
+            }
+            Console.WriteLine(stringStart);
+            Console.WriteLine(separator + fio + separator);
+            Console.Write(stringStart);
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(delimiter);
+            }
+            Console.WriteLine(stringStart);
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
         }
     }
 }
