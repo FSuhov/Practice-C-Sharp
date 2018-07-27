e08f667e2d2531437c9f43cd56e04e4aeb215a06
Alex Brylov <aksu@ukr.net>
2018-07-27 17:22:25 +0300

    Project for Task 3 - Inheritance - has been created. Task 1 completed

---
+++ TASK 3/Inheritance/01Printer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Создайте класс Printer.
+В теле класса создайте метод void Print(string value), который выводит на экран значение аргумента.
+Реализуйте возможность того, чтобы в случае наследования от данного класса других классов, и вызове соответствующего метода их экземпляра,
+строки, переданные в качестве аргументов методов, выводились разными цветами.
+Обязательно используйте приведение типов.
+*/
+
+namespace _01Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer[] printers = { new Printer(),
+                                   new BluePrinter(),
+                                   new RedPrinter()
+                                 };
+            foreach(Printer printer in printers)
+            {
+                printer.Print("To be or not to be");
+            }
+
+            BluePrinter blueprinter = printers[1] as BluePrinter;
+            RedPrinter redprinter = printers[2] as RedPrinter;
+
+            blueprinter.Print("Синий принтер приведенный к синему");
+            redprinter.Print("Красный принтер приведенный к красному");
+        }
+    }
+
+    class Printer
+    {
+        public Printer (){}
+
+        public void Print(string value)
+        {           
+            Console.WriteLine(value);
+        }
+    }
+
+    class BluePrinter : Printer
+    {       
+        public void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+
+    class RedPrinter : Printer
+    {       
+
+        public void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+}
